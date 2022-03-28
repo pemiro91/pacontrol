bff39d940ff710f4e6db4b3569f4a60c9af80a6b
pemiro91 <pemiroo91@gmail.com>
2022-03-27 21:52:59 -0400

    Update dominio

--- hccontrol/settings.py
+++ hccontrol/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'hccontrol.com', 'www.hccontrol.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'hccontrolp.com', 'www.hccontrolp.com']
 
 
 # Application definition
